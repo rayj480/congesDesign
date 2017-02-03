2aa55ef589f307e6552b7a7bfd0498e9efa7a8fc
Jonathan CARI <jonathan.cari@tryade.fr>
2017-02-03 18:05:42 +0100

    Ajout de autoprefixer et ajustement CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass'); 
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('style', function (){
     gulp.src('./src/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./dist/css'));
 });
 
